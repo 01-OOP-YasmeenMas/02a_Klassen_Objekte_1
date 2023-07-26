849ba2832511af939826088b7cb3f2e978e680fe
Yasmeen <yasmeenmas@gmx.de>
2023-07-26 10:05:31 +0200

    Erzeugen von Objekten ohne Referenz

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,6 +5,10 @@ public class App {
     public static void main(String[] args) {
 
 
+        //Erzeugen von Objekten ohne Referenz
+        System.out.println(new Cat());
+        System.out.println(new Cat());
+
     }
 
    
